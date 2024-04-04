70f61f75991a8358a309d1f7099e3e580d47105e
Valery <verelly.v@gmail.com>
2024-04-04 19:59:54 +0300

    Add test line

--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,6 @@ const rootReducer = combineReducers({
 
 const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
 export const store = createStore(rootReducer, middleware)
-github.com/
 
 // store.subscribe(() => {
 //     console.log('**** Store state changed: ****')
@@ -19,5 +18,5 @@ github.com/
 //     console.log('*******************************')
 // })
 
-
+console.log('test');
 
