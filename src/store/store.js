260a96a27ebb6a53b62c2cb2f85b10d39487f6d0
verely <44124213+verely@users.noreply.github.com>
2024-04-04 19:58:07 +0300

    Update store.js
    Remove useless line

--- src/store/store.js
+++ src/store/store.js
@@ -11,7 +11,6 @@ const rootReducer = combineReducers({
 
 const middleware = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : undefined
 export const store = createStore(rootReducer, middleware)
-github.com/
 
 // store.subscribe(() => {
 //     console.log('**** Store state changed: ****')
