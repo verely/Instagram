10fc3c855a6ffbafe4394ec732e7d35ba68e9e07
RafaelNavon <raffi158@gmail.com>
2024-04-04 20:06:32 +0300

    Remove service worker

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client'
 // import { HashRouter as Router } from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { store } from './store/store'
 import { RootCmp } from './RootCmp'
 import './assets/styles/main.scss'
@@ -15,8 +14,3 @@ root.render(
         </Router>
     </Provider>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
