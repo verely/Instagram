f149427cdfbb087623ada73452b1857689d1ac13
Valery <verelly.v@gmail.com>
2024-07-22 11:34:19 +0300

    Replace user data with Alice and Bob placeholders following cryptographic conventions.

--- src/data-model/user.js
+++ src/data-model/user.js
@@ -1,25 +1,25 @@
 export const user = {
-    "_id": "u101",
-    "userName": "Muko",
-    "password": "mukmuk",
-    "fullName": "Muki Muka",
-    "imgUrl": "http://some-img",
-    "bio": "I like falafel, I am from Netanya",
-    "followingCount": 100,
-    "following": [
-      {
-        "_id": "u106",
-        "fullName": "Dob",
-        "imgUrl": "http://some-img"
-      }
-    ],
-    "followersCount": 100,
-    "followers": [
-      {
-        "_id": "u105",
-        "fullName": "Bob",
-        "imgUrl": "http://some-img"
-      }
-    ],
-    "savedStoryIds": ["s104", "s111", "s123"]
-  }
+  "_id": "u101",
+  "userName": "Alice",
+  "password": "123",
+  "fullName": "Alice Smith",
+  "imgUrl": "https://professional-image.com/alicesmith.jpg",
+  "bio": "Tech Enthusiast & Cryptography Explorer.",
+  "followingCount": 150,
+  "following": [
+    {
+      "_id": "u102",
+      "fullName": "Bob Johnson",
+      "imgUrl": "https://professional-image.com/bobjohnson.jpg"
+    }
+  ],
+  "followersCount": 200,
+  "followers": [
+    {
+      "_id": "u103",
+      "fullName": "Mallory Brown",
+      "imgUrl": "https://professional-image.com/mallorybrown.jpg"
+    }
+  ],
+  "savedStoryIds": ["s104", "s111", "s123"]
+}
